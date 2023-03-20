0f2719613a71aa0425cb8e0ac4ba468d512a48d0
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-20 19:43:59 +0545

    layout for navbar and footer on each page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
 import React from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter ,Outlet} from 'react-router-dom'
 import Home from "./page/home/Home"
 import Products from "./page/products/Products"
 import Product from "./page/product/Product"
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 const App = () => {
+
+
+  const Layout = ()=>{
+    return(
+    <div className='app'>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+
+    </div>
+    )
+  }
 
 
 
   const router = createBrowserRouter([
     {
       path:"/",
-      element:<Home/>
+      element:<Layout/>,
+      children:[
+        {
+          path:"/",
+          element:<Home/>
+        },
+        {
+          path:"/products/:id",
+          element:<Products/>
+        },
+        {
+          path:"/product/:id",
+          element:<Product/>
+        }
+      ]
 
-    },
-    {
-      path:"/products/:id",
-      element:<Products/>
-    },
-    {
-      path:"/product/:id",
-      element:<Product/>
     }
   ])
 
