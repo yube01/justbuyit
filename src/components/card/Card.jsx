9161efb00c52b7803824b09cf4a465f991fe643f
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-23 12:18:38 +0545

    card design complete

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import "./card.scss"
-const Card = () => {
+import { Link } from 'react-router-dom'
+const Card = ({item}) => {
   return (
-    <div>Card</div>
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
+        <div className="image">
+          
+          <img
+            src={
+              item.img1
+            }
+            alt=""
+            className="mainImg"
+          />
+          <img
+            src={
+              item.img2
+            }
+            alt=""
+            className="secondImg"
+          />
+        </div>
+        <h2>{item.title}</h2>
+        <div className="prices">
+          <h3>${item.oldPrice}</h3>
+          <h3>${item.price}</h3>
+        </div>
+      </div>
+    </Link>
   )
 }
 
