0f36662be02ec0651540dbfc670a40f7c48eb950
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-23 12:36:12 +0545

    grid added

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from "../../components/slider/Slider"
 import Feature from '../../components/feature/Feature'
+import Categories from '../../components/categories/Categories'
 
 const Home = () => {
   return (
     <div className="home">
       <Slider/>
       <Feature type="Feature"/>
+      <Categories/>
       <Feature type="Trending"/>
     </div>
   )
