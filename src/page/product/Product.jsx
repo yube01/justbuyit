74ebca46d040044b2b3629effc89708c87ac8eca
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-26 07:24:56 +0545

    product left side image selector added

--- src/page/product/Product.jsx
+++ src/page/product/Product.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./product.scss"
 const Product = () => {
+
+
+  const [selectedImg , setSelectedImg] = useState(1)
+
+  const images = [
+    "https://images.pexels.com/photos/2043590/pexels-photo-2043590.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1381556/pexels-photo-1381556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ]
+
   return (
-    <div className="pro">
-      
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img src={images[0]} alt="" onClick={e=>setSelectedImg(0)} />
+          <img src={images[1]} alt="" onClick={e=>selectedImg(1)}/>
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+        </div>
+
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
