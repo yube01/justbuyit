dcd3787b9cf2b91254a5f362e23df47cb4a0fe52
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-27 16:04:53 +0545

    cart structure created

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,8 +1,65 @@
 import React from 'react'
 import "./cart.scss"
 const Cart = () => {
+
+
+
+    const data = [
+        {
+            id:1,
+            img1:"https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=495&q=80",
+            img2:"https://images.unsplash.com/photo-1493704099487-36e3c0412f6e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1154&q=80",
+            title:"yellow",
+            isNew:false,
+            desc:"new product",
+            oldPrice:12,
+            price:38
+        },
+        {
+            id:1,
+            img1:"https://images.unsplash.com/photo-1601762603339-fd61e28b698a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            img2:"https://images.unsplash.com/photo-1654806389957-f8ca19bb7b1a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+            title:"white",
+            desc:"new product",
+            isNew:false,
+            oldPrice:149,
+            price:100
+        }
+    ]
+
   return (
-    <div>Cart</div>
+    <div className="cart">
+      <h1>Products in your cart</h1>
+      {data?.map((item) => (
+        <div className="item" key={item.id}>
+          <img src={item?.img1} alt="" />
+          <div className="details">
+            <h1>{item?.title}</h1>
+            <p>{item.desc?.substring(0,100)}</p>
+            <div className="price">
+               ${item.price}
+            </div>
+          </div>
+          
+          <div className="delete">
+          <span class="material-symbols-outlined">
+delete
+</span>
+          
+          </div>
+          
+        </div>
+      ))}
+      <div className="total">
+        <span>SUBTOTAL</span>
+        <span>$456</span>
+      </div>
+      <button>PROCEED TO CHECKOUT</button>
+      <span className="reset">
+        Reset Cart
+      </span>
+      
+    </div>
   )
 }
 
