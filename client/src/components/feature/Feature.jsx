66ab82f09384029bb2db00c4a44dd8a3920df9fd
yube01 <114811769+yube01@users.noreply.github.com>
2023-03-28 07:17:43 +0545

    axios

--- client/src/components/feature/Feature.jsx
+++ client/src/components/feature/Feature.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./feature.scss"
 import Card from '../card/Card'
+import axios from "axios"
 const Feature = ({type}) => {
     const data = [
         {
@@ -42,6 +43,28 @@ const Feature = ({type}) => {
 
 
     ]
+
+    const [ products, setProducts] = useState([])
+
+    useEffect(()=>{
+      const fetchData = async ()=>{
+        try {
+          const data = await axios.get( "http://localhost:1337/api/products",{
+            headers:{
+              Authorization: "bearer" + "1559c3970bce0c42dcbeff8ed45cf0e1cd8b3772db90ea7c0aa3c45d3ec38e9fd86c742a2c57efb56c98ab851b32cd1b5e99aba982b7890dbe7f09838ad0aa6b57165f66cd0e758ba5130b73867ecfb6d1a15f81cd3aee8e335d825e56540bc89d1e2a3e28f2db3e9a6ce16aab1cd64025ebae16bb94ffb7b0780f47faa68866"
+            }
+            
+          })
+          console.log(data)
+          
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      fetchData()
+    },[])
+
+
   return (
     <div className="feature">
          <div className="top">
