291aaa45034f0e7b0ad9c1f2cd296cfe35d4f4a2
yube01 <114811769+yube01@users.noreply.github.com>
2023-04-02 07:01:08 +0545

    strapi

--- server/src/api/order/controllers/order.js
+++ server/src/api/order/controllers/order.js
@@ -7,6 +7,50 @@ const stripe = require("stripe")(process.env.STRIPE_KEY)
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::order.order',((strapi)=>{
+module.exports = createCoreController('api::order.order',({strapi})=>({
+  async create(ctx){
+    const {products} = ctx.request.body;
 
-}));
+    const lineItems = await Promise.all(
+      products.map(async(item)=>
+      {const item = await strapi.service("api:product.product").findOne(item.id)
+
+      return{
+        price_data:{
+          currency:"usd",
+          product_data:{
+            name:item_title
+          },
+          unit_amount:item.price*100
+        },
+        qunatity:item.qunatity
+      }
+    })
+    )
+
+    try {
+      const session = stripe.checkout.create({
+        mode:"payment",
+        success_url:`${process.env.CLIENT_URL}?success=true`,
+        cancel_url:`${process.env.CLIENT_URL}?success=false`,
+        line_items: lineItems,
+        shipping_address_collection:{allowed_countries: ["NEP","US"]},
+        payment_method_type: ["card"]
+      })
+
+      await strapi.service("api::order:order").create({
+        data:{
+          products,
+          strapiId: session.id
+        }
+      })
+
+      return{ stripeSession: session}
+
+    } catch (error) {
+      ctx.response.status = 500;
+      return error;
+
+    }
+  }
+}))
